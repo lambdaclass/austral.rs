bdfa08ec1a6c065db126a4583274510e4f031d42
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-12-28 19:05:02 +0100

    Add extremely simple compiler skeleton. Make an empty program work.

--- lib/austral_lib/examples/empty_program.rs
+++ lib/austral_lib/examples/empty_program.rs
@@ -1,19 +1,23 @@
 use austral_lib::ast::ModuleDef;
 use chumsky::Parser;
-use melior::Context;
+use melior::{dialect::DialectRegistry, Context};
 
 fn main() {
     let tokens =
-        austral_lib::lexer::lex(include_str!("../../../programs/examples/hello_world.aum"))
+        austral_lib::lexer::lex(include_str!("../../../programs/examples/empty_program.aum"))
             .map(|(token, _span)| token)
             .collect::<Result<Vec<_>, _>>()
             .unwrap();
-    dbg!(&tokens);
-
     let ast = ModuleDef::parser().parse(&tokens).into_result().unwrap();
-    dbg!(&ast);
 
     let context = Context::new();
+    context.append_dialect_registry(&{
+        let dialect_registry = DialectRegistry::new();
+        melior::utility::register_all_dialects(&dialect_registry);
+        dialect_registry
+    });
+    context.load_all_available_dialects();
+
     let prog = austral_lib::compiler::compile(&context, &ast, &[]);
 
     println!("{}", prog.as_operation());
---
+++ lib/austral_lib/src/compiler.rs
@@ -0,0 +1,122 @@
+use crate::ast::{
+    AtomicExpr, Expression, FunctionDef, ModuleDecl, ModuleDef, ModuleDefItem, TypeSpec,
+};
+use melior::{
+    dialect::{arith, func},
+    ir::{
+        attribute::{IntegerAttribute, StringAttribute, TypeAttribute},
+        r#type::{FunctionType, IntegerType},
+        Block, Location, Module, Region, Type, Value,
+    },
+    Context,
+};
+use std::{borrow::Cow, ops::Deref};
+
+pub struct BuildContext<'c> {
+    context: &'c Context,
+    module: Module<'c>,
+}
+
+impl<'c> Deref for BuildContext<'c> {
+    type Target = Context;
+
+    fn deref(&self) -> &Self::Target {
+        self.context
+    }
+}
+
+pub fn compile<'c>(
+    context: &'c Context,
+    root: &ModuleDef,
+    _interfaces: &[ModuleDecl],
+) -> Module<'c> {
+    let build_context = BuildContext {
+        context,
+        module: Module::new(Location::unknown(context)),
+    };
+
+    for module_item in &root.contents {
+        match module_item {
+            ModuleDefItem::Function(data) => compile_function(&build_context, data),
+            _ => todo!(),
+        }
+    }
+
+    build_context.module
+}
+
+fn compile_function(ctx: &BuildContext<'_>, root: &FunctionDef) {
+    let region = Region::new();
+
+    let block = region.append_block(Block::new(&[]));
+    for stmt in &root.body {
+        match stmt {
+            crate::ast::Statement::Discard(expr) => {
+                let _expr = process_expr(expr);
+                todo!()
+            }
+            crate::ast::Statement::Return(expr) => {
+                let expr = process_expr(expr);
+                let value = build_expr(ctx, &block, &expr);
+                block.append_operation(func::r#return(&[value], Location::unknown(ctx)));
+            }
+            _ => todo!(),
+        }
+    }
+
+    let arg_types = root
+        .params
+        .iter()
+        .map(|_param| todo!())
+        .collect::<Vec<Type>>();
+    let ret_type = match &root.ret_type {
+        TypeSpec::Simple { name } => match name.name.as_str() {
+            "ExitCode" => IntegerType::new(ctx, 32).into(),
+            _ => todo!(),
+        },
+        _ => todo!(),
+    };
+
+    ctx.module.body().append_operation(func::func(
+        ctx,
+        StringAttribute::new(ctx, &root.name.name),
+        TypeAttribute::new(FunctionType::new(ctx, &arg_types, &[ret_type]).into()),
+        region,
+        &[],
+        Location::unknown(ctx),
+    ));
+}
+
+fn process_expr(expr: &Expression) -> Cow<Expression> {
+    if let Expression::Atomic(AtomicExpr::FnCall(expr)) = expr {
+        match expr.target.name.as_str() {
+            "ExitSuccess" => return Cow::Owned(Expression::Atomic(AtomicExpr::ConstInt(0))),
+            "ExitFailure" => return Cow::Owned(Expression::Atomic(AtomicExpr::ConstInt(1))),
+            _ => {}
+        }
+    }
+
+    Cow::Borrowed(expr)
+}
+
+fn build_expr<'c, 'b>(
+    ctx: &'c BuildContext<'c>,
+    block: &'b Block<'c>,
+    expr: &Expression,
+) -> Value<'c, 'b> {
+    match expr {
+        Expression::Atomic(expr) => match expr {
+            AtomicExpr::ConstInt(value) => block
+                .append_operation(arith::constant(
+                    ctx,
+                    IntegerAttribute::new(*value as i64, IntegerType::new(ctx, 32).into()).into(),
+                    Location::unknown(ctx),
+                ))
+                .result(0)
+                .unwrap()
+                .into(),
+            _ => todo!(),
+        },
+        Expression::Compound(_) => todo!(),
+    }
+}
