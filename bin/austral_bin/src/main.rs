22b9f99aa94aa682c83a8360a3cb2a7bcc404025
gabrielbosio <gabrielbosio95@gmail.com>
2023-12-28 15:44:00 -0300

    Add CLI to print AST

--- bin/austral_bin/src/main.rs
+++ bin/austral_bin/src/main.rs
@@ -1,29 +1,22 @@
-use clap::Parser;
 use austral_lib::ast::ModuleDef;
-//use chumsky::Parser;
+use chumsky::Parser as _;
+use clap::Parser;
 use std::fs;
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the input file
-    #[arg(short, long)]
-    input_file: String,
+fn main() {
+    let args = CmdLine::parse();
+    let input_file = fs::read_to_string(args.input_path).unwrap();
 
-    /// Name of the output file path
-    #[arg(short, long, default_value_t = String::from("output.txt"))]
-    output_path: String,
+    let tokens = austral_lib::lexer::lex(input_file.as_str())
+        .map(|(token, _span)| token)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+
+    let ast = ModuleDef::parser().parse(&tokens).into_result().unwrap();
+    dbg!(&ast);
 }
 
-fn main() {
-    let args = Args::parse();
-    let source_code = fs::read_to_string(args.input_file).unwrap();
-
-/*     let tokens =
-        austral_lib::lexer::lex(&source_code)
-            .map(|(token, _span)| token)
-            .collect::<Result<Vec<_>, _>>()
-            .unwrap();
-    let ast = ModuleDef::parser().parse(&tokens).into_result().unwrap();
-    dbg!(ast); */
+#[derive(Clone, Debug, Parser)]
+struct CmdLine {
+    input_path: String,
 }
