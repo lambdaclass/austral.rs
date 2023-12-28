bb6d77c2e2488ba92bb4e0523581a0b9ace6dba9
juanbono <juanbono94@gmail.com>
2023-12-28 09:10:40 -0300

    add tests for literals

--- lib/austral_lib/src/ast/literals.rs
+++ lib/austral_lib/src/ast/literals.rs
@@ -52,3 +52,53 @@ pub fn literal_str<'a>() -> impl Clone + Parser<'a, &'a [Token<'a>], Cow<'a, str
         })
     })
 }
+
+#[cfg(test)]
+mod literals_parser_tests {
+    use super::*;
+    use crate::lexer::Token;
+    use std::borrow::Cow;
+
+    #[test]
+    fn test_literal_str() {
+        let hello_world_str = vec![Token::String(Cow::Borrowed("hello world"))];
+        assert_eq!(
+            literal_str().parse(&hello_world_str).unwrap(),
+            Cow::Borrowed("hello world")
+        );
+
+        let empty_str = vec![Token::String(Cow::Borrowed(""))];
+        assert_eq!(literal_str().parse(&empty_str).unwrap(), Cow::Borrowed(""));
+    }
+
+    #[test]
+    fn test_literal_u64() {
+        assert_eq!(literal_u64().parse(&[Token::Decimal(10)]).unwrap(), 10);
+        assert_eq!(literal_u64().parse(&[Token::Decimal(0)]).unwrap(), 0);
+        assert_eq!(literal_u64().parse(&[Token::Decimal(1)]).unwrap(), 1);
+    }
+
+    #[test]
+    fn test_literal_f64() {
+        assert_eq!(literal_f64().parse(&[Token::Float(10.0)]).unwrap(), 10.0);
+        assert_eq!(literal_f64().parse(&[Token::Float(0.0)]).unwrap(), 0.0);
+        assert_eq!(literal_f64().parse(&[Token::Float(1.0)]).unwrap(), 1.0);
+        assert_eq!(literal_f64().parse(&[Token::Float(-1.0)]).unwrap(), -1.0);
+    }
+
+    #[test]
+    fn test_literal_char() {
+        assert_eq!(literal_char().parse(&[Token::Char('a')]).unwrap(), 'a');
+    }
+
+    #[test]
+    fn test_literal_bool() {
+        assert_eq!(literal_bool().parse(&[Token::True]).unwrap(), true);
+        assert_eq!(literal_bool().parse(&[Token::False]).unwrap(), false);
+    }
+
+    #[test]
+    fn test_literal_nil() {
+        assert_eq!(literal_nil().parse(&[Token::Nil]).unwrap(), ());
+    }
+}
