2cc17fc17476d990b279ade66c33cb4a5e92ad66
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-12-28 19:36:30 +0100

    Make the span compatible with chumsky.

--- lib/austral_lib/src/span.rs
+++ lib/austral_lib/src/span.rs
@@ -1,10 +1,39 @@
-pub struct Position {
-    pub line: usize,
-    pub column: usize,
+use std::{num::NonZeroUsize, ops::Range, path::PathBuf};
+
+#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct SpanContext {
+    pub path: Option<PathBuf>,
 }
 
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct SpanLocation {
+    pub line: NonZeroUsize,
+    pub column: NonZeroUsize,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Span {
-    pub filename: String,
-    pub startp: Position,
-    pub endp: Position,
+    pub context: SpanContext,
+    pub range: Range<SpanLocation>,
+}
+
+impl chumsky::span::Span for Span {
+    type Context = SpanContext;
+    type Offset = SpanLocation;
+
+    fn new(context: Self::Context, range: Range<Self::Offset>) -> Self {
+        Self { context, range }
+    }
+
+    fn context(&self) -> Self::Context {
+        self.context.clone()
+    }
+
+    fn start(&self) -> Self::Offset {
+        self.range.start
+    }
+
+    fn end(&self) -> Self::Offset {
+        self.range.end
+    }
 }
